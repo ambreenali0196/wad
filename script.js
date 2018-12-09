d3b0e341d0ef7e556f19a282f6f85889fcf7091a
ambreenali0196 <44544178+ambreenali0196@users.noreply.github.com>
2018-12-09 02:33:46 -0800

    quiz4

--- script.js
+++ script.js
@@ -19,38 +19,64 @@ var questions = [{
         "for",
         "none of the above"],
     correctAnswer : 1
-} ,{
-    question : "who is the national animal of PAKISTAN",
-    choices : [ "select",
-        "markhor",
-        "cat",
-        "none of the above"],
-    correctAnswer : 0
-} ];
-currentQuestion=0;
+}];
+
 var currentQuestion = 0;
 var correctAnswers = 0;
 var quizOver = false;
 displayCurrentQuestion();
 document.getElementById("quiz-message").style.display = 'none';
 function displayNext() {
-    document.getElementById("question").innerHTML=questions[currentQuestion].question + questions[currentQuestion].choices;
-    currentQuestion++;
+    if(!quizOver){
+        var selectedValue = null;
+        if(document.querySelector('input[name="dq"]:checked') !== null)
+            selectedValue = document.querySelector('input[name="dq"]:checked').value;
+        if (selectedValue == null) {
+            document.getElementById("quiz-message").innerText = "Please selected an answer"
+            document.getElementById("quiz-message").style.display = 'block';
+        } else{
+            document.getElementById("quiz-message").style.display = 'none';
+            if(selectedValue == questions[currentQuestion].correctAnswer){
+                correctAnswers++;
+            }
+            currentQuestion++;
+            if(currentQuestion < questions.length){
+                displayCurrentQuestion();
+            }
+            else {
+                displayScore();
+                document.getElementById("next-btn").innerText = "Play Again?"
+                quizOver = true;
+            }
+        }
+    } else {
+        quizOver = false;
+        document.getElementById("next-btn").innerText = "Next Question";
+        resetQuiz();
+        displayCurrentQuestion();
+        hideScore();
+    }
 
-    /*Write your code here */
 }
 
 function displayCurrentQuestion() {
-    document.getElementById("question").innerHTML=questions[currentQuestion].question + questions[currentQuestion].choices;
-    currentQuestion++;
-     /*Write your code here */
+    var question = questions[currentQuestion].question;
+    var questionId = document.getElementById("question");
+    var choiceList = document.getElementById("choice-list");
+    var numChoices = questions[currentQuestion].choices.length;
+
+    questionId.innerText = question;
+    choiceList.innerHTML = "";
+    var choice;
+    for(var i=0; i<numChoices; i++){
+        choice = questions[currentQuestion].choices[i];
+        choiceList.innerHTML += "<li><input type='radio' value='"+i+"' name='dq'>" + choice + "</li>";
+    }
 }
 
 function resetQuiz() {
-
     currentQuestion = 0;
     correctAnswers = 0;
-    displayCurrentQuestion();
     hideScore();
 }
 function displayScore() {
